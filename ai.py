4f1f82da842b8d16348f2de29d8cf66a1ebc9f69
rockodagaer <76546343+rockodagaer@users.noreply.github.com>
2022-04-22 10:34:35 +0200

    ai.py
    This is the AI

---
+++ ai.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+class Model:
+    # make a model that takes inputs and outputs and generates a new output based on the inputs and outputs
+    def __init__(self, inputs, outputs):
+        self.inputs = inputs
+        self.outputs = outputs
+        self.weights = np.random.rand(inputs, outputs)
+        self.bias = np.random.rand(outputs)
+        self.learning_rate = 0.1
+    
+    def predict(self, inputs):
+        return np.dot(inputs, self.weights) + self.bias
+
+    def train(self, inputs, expected_outputs):
+        predicted_outputs = self.predict(inputs)
+        error = predicted_outputs - expected_outputs
+        self.weights -= self.learning_rate * np.dot(inputs.T, error)
+        self.bias -= self.learning_rate * np.sum(error, axis=0)
+
+    def test(self, inputs, expected_outputs):
+        predicted_outputs = self.predict(inputs)
+        error = predicted_outputs - expected_outputs
+        return np.mean(np.abs(error))
