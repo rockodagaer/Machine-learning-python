85d0f0bf3ab9f2b4f20dd2aae343f65912e83a29
rockodagaer <76546343+rockodagaer@users.noreply.github.com>
2022-06-26 13:06:28 +0200

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,18 +1,20 @@
 import numpy as np
 
-class Model:
-    # make a model that takes inputs and outputs and generates a new output based on the inputs and outputs
+# this is old and does not resemble anything special
+# it only takes numbers. if you want it to generate text or something you are gonna have to convert text to numbers and numbers to text
+# some of this was also made of the closed beta version of github copilot which released a full version now
+class Model: # the main ai
     def __init__(self, inputs, outputs):
-        self.inputs = inputs
-        self.outputs = outputs
-        self.weights = np.random.rand(inputs, outputs)
-        self.bias = np.random.rand(outputs)
-        self.learning_rate = 0.001
+        self.inputs = inputs # input size
+        self.outputs = outputs # output size
+        self.weights = np.random.rand(inputs, outputs) # make weights
+        self.bias = np.random.rand(outputs) # make biases
+        self.learning_rate = 0.001 # set learning rate (the lower the more precise it learns)
     
-    def predict(self, inputs):
-        return np.dot(inputs, self.weights) + self.bias
+    def predict(self, inputs): # predict a number
+        return np.dot(inputs, self.weights) + self.bias # return the predicted output (numbers only)
 
-    def train(self, inputs, expected_outputs, epochs=1):
+    def train(self, inputs, expected_outputs, epochs=1): # train the model
         for epoch in range(epochs):
             predicted_outputs = self.predict(inputs)
             error = predicted_outputs - expected_outputs
@@ -21,10 +23,10 @@ class Model:
             self.weights -= self.learning_rate * gradient
             self.bias -= self.learning_rate * np.mean(error, axis=0)
 
-    def test(self, inputs, expected_outputs):
+    def test(self, inputs, expected_outputs): # test how good it is at predicting the right outputs
         predicted_outputs = self.predict(inputs)
         error = predicted_outputs - expected_outputs
-        return np.mean(np.abs(error))
+        return np.mean(np.abs(error)) # return the amount of errors ( warning it doesnt return the exact amount of errors )
     
-    def learn_rate(self, learning_rate):
+    def learn_rate(self, learning_rate): # set learning rate
         self.learning_rate = learning_rate
