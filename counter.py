a848a12d6f1fee435f5e5e2222269070366c4a87
rockodagaer <76546343+rockodagaer@users.noreply.github.com>
2022-06-26 13:14:26 +0200

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,10 +1,12 @@
 from ai import Model
+# this is an example of how to make an accurate counter using the ai.
+# ofcourse it can do much more than this. i made a full calculator with it but you could teach it to do anythind you want
 
-model = Model(2, 1)
-model.learning_rate(0.001)
-inputs = [[1, 1], [1, 2], [2, 2], [4, 7], [10, 10], [32, 57], [1, 57]]
-outputs = [[2], [3], [4], [11], [20], [89], [58]]
-model.train(np.array(inputs), np.array(outputs), epochs=5000)
-output = model.predict(np.array([[2, 57]]))
-print(output)
-print(round(output[0][0]))
+model = Model(1, 1) # the model has 1 input and 1 output so you set it to 1, 1
+model.learning_rate(0.001) # set the training data to something low
+inputs = [[0], [1], [2], [3], [4], [5], [6], [7]] # inputs, the outputs will be the same item in outputs, warning: it doesnt take lists of lists
+outputs = [[1], [2], [3], [4], [5], [6], [7], [8]] # outputs
+model.train(np.array(inputs), np.array(outputs), epochs=5000) # train the model with 50000 epcochs will take only a min ( more for better output )
+output = model.predict(np.array([[2]])) # predict the output as a float
+print(output) # print the output as a float
+print(round(output[0])) # return it with round() not int() because it relies on that you round the number to make it perfect
